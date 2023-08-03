d78dc319c6e8d0de2f5cb9df23df650d55fe95b0
Sandyou <x8804567@gmail.com>
2023-08-03 14:55:39 +0800

    feat: 0803-2 Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const abc = 1234;
+const abc = 12345;
 
 app.get('/', (req, res) => res.send('Hello Sandy\'s cloudformation codepipline World!'));
 
